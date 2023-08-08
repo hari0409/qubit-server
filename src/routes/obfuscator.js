79f87f164d1c3cd678d0606cd9f182e0aafa36ae
hari0409 <hari626007@gmail.com>
2023-08-08 11:22:43 +0530

    new obfus

--- src/routes/obfuscator.js
+++ src/routes/obfuscator.js
@@ -1,12 +1,24 @@
+const { exec } = require("child_process");
 const { upload } = require("../controllers/obfuscator");
-
+const path = require("path")
 const router = require("express").Router();
+const colors = require("colors")
+
+const scriptPath = path("../scripts/main.py")
+
 
 //Create a post
 router.post("/upload", async (req, res, next) => {
     try {
         const { type, path } = req.body;
-        console.log(type,path);
+        exec(`python3 ${scriptPath} ${path}`, (err, stdout, stderr) => {
+            if (err) {
+                console.log(`${err}`.red);
+            }
+            else {
+                console.log(`${stdout}`.green);
+            }
+        })
         res.status(200).json({ status: "success" });
     } catch (error) {
         next(error);
--- src/scripts/main.py
+++ src/scripts/main.py
@@ -2,12 +2,13 @@ from PIL import Image
 import numpy as np
 import pickle
 import cv2
+import sys
 from doctest import OutputChecker
 from steganography import encode
 from convert_to_binary import str_to_binary
 from paillier import Encrypt, Decrypt, generate_keys,image_encryption,show_encrypted_image
 
-file_path="test.py" #response from node js server
+file_path=sys.argv[1] #response from node js server
 folder=file_path.split("\\")
 f_name=folder.pop()
 type=f_name.split(".")[1]
@@ -15,7 +16,7 @@ print(folder)
 n=len(folder)                  
 folder_path="\\".join(folder)
 
-file=open(file_path,'r')
+"""file=open(file_path,'r')
 str_to_conv=file.read()
 print(str_to_conv)
 
@@ -26,7 +27,7 @@ print(bin_result)
 if(type!="png" or type!="jpg" or type!="jpeg"): #if the file type is not an image file we will embed it into one
 	image_path="stego.jpg" #response from node js server
 	encode(image_path,bin_result,folder_path) #saves an image file named "encoded_image.png" which is our stego file
-
+"""
 
 publickey, privatekey = generate_keys()
 print(publickey.__repr__())
@@ -41,6 +42,7 @@ encrypt_image.shape
 enc_img=show_encrypted_image(encrypt_image,folder_path)
 
 import matplotlib.pyplot as plt
+
 img = plt.imread(folder_path+"\\encrypted_image.png")
 gray = np.mean(img, axis=-1)
 
